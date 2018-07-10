b74b52a56e735b6700f82ded459aa6c7421d9b98
henryyu92 <1401802687@qq.com>
2018-07-10 17:36:26 +0800

    'init'

---
+++ microservice-provider-user/src/main/java/org/mooc/cloud/controller/UserController.java
@@ -0,0 +1,20 @@
+package org.mooc.cloud.controller;
+
+import org.mooc.cloud.entity.User;
+import org.mooc.cloud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable("id") Long id){
+        return userService.getUser(id);
+    }
+}
