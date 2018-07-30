a0c75ce9e0acf9ea7121b1c8de8d5125d9da60b1
henryyu92 <1401802687@qq.com>
2018-07-30 21:30:06 +0800

    hystrix-dashboard

---
+++ microservice-hystrix-dashboard/src/main/java/org/mooc/cloud/HystrixDarshbaordApplication.java
@@ -0,0 +1,20 @@
+package org.mooc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDarshbaordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDarshbaordApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
