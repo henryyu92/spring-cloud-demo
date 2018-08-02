748266a0bcdf24fc33e46dab2d7753aa7c449e35
henryyu92 <1401802687@qq.com>
2018-08-02 14:25:34 +0800

    turbine

--- microservice-consumer-movie-config/src/main/java/org/mooc/cloud/MicroserviceConsumerMovieConfigApplication.java
+++ microservice-consumer-movie-config/src/main/java/org/mooc/cloud/MicroserviceConsumerMovieConfigApplication.java
@@ -2,12 +2,14 @@ package org.mooc.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class MicroserviceConsumerMovieConfigApplication {
 
 	public static void main(String[] args) {
