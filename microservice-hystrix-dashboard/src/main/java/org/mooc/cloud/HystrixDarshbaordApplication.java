4b3e70c6d9b10caf763c34b62e0f78da72cabb1e
henryyu92 <1401802687@qq.com>
2018-08-02 13:45:05 +0800

    modify

--- microservice-hystrix-dashboard/src/main/java/org/mooc/cloud/HystrixDarshbaordApplication.java
+++ microservice-hystrix-dashboard/src/main/java/org/mooc/cloud/HystrixDarshbaordApplication.java
@@ -2,12 +2,9 @@ package org.mooc.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,10 +14,5 @@ public class HystrixDarshbaordApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HystrixDarshbaordApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
 	}
 }
