0ca354231c46be90570f352c25022a626088e84e
henryyu92 <1401802687@qq.com>
2018-09-06 12:01:57 +0800

    modify

--- microservice-consumer-movie-config/src/main/java/org/mooc/cloud/interceptor/RequestCheckInterceptor.java
+++ microservice-consumer-movie-config/src/main/java/org/mooc/cloud/interceptor/RequestCheckInterceptor.java
@@ -1,13 +1,20 @@
 package org.mooc.cloud.interceptor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 
 public class RequestCheckInterceptor extends HandlerInterceptorAdapter {
+
+    @Value("requestCheckInterceptor.includePattens")
+    private List<String> includePattens;
+    @Value("requestCheckInterceptor.excludePattens")
+    private List<String> excludePattens;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
