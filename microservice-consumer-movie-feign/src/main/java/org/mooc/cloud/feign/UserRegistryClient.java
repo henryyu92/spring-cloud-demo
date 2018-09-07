4864f1b8a51fca5acb4f4406baae1bb1f725750f
henryyu92 <1401802687@qq.com>
2018-09-07 09:51:10 +0800

    modify

--- microservice-consumer-movie-feign/src/main/java/org/mooc/cloud/feign/UserRegistryClient.java
+++ microservice-consumer-movie-feign/src/main/java/org/mooc/cloud/feign/UserRegistryClient.java
@@ -3,7 +3,6 @@ package org.mooc.cloud.feign;
 import org.mooc.cloud.entity.User;
 import org.mooc.cloud.feign.impl.UserRegistryFallback;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
