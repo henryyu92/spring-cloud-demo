c45ca72dc30048964773d3bd8dc7980e8fadeb7f
henryyu92 <1401802687@qq.com>
2020-01-10 23:25:31 +0800

    update

--- microservice-gateway/src/main/java/org/mooc/cloud/MicroServiceGatewayApplication.java
+++ microservice-gateway/src/main/java/org/mooc/cloud/MicroServiceGatewayApplication.java
@@ -1,22 +1,16 @@
 package org.mooc.cloud;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+/**
+ * @author
+ */
 @SpringBootApplication
-@EnableEurekaServer
 public class MicroServiceGatewayApplication {
 
-	/**
-	 * Eureka is the Netflix Service Discovery Server and Client.
-	 * The server can be configured and deployed to be highly available,
-	 * with each server replicating state about the registered services to the others.
-	 *
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		SpringApplication.run(MicroServiceGatewayApplication.class, args);
+		new SpringApplicationBuilder(MicroServiceGatewayApplication.class).run(args);
 	}
 
 }
