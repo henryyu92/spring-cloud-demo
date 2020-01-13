a2b1046dfd4d05efc2104af7cb9ca8ad1672bdc2
yuhui <yuhui@evergrande.com>
2020-01-13 15:55:42 +0800

    update

--- microservice-gateway/src/main/java/org/mooc/cloud/MicroServiceGatewayApplication.java
+++ microservice-gateway/src/main/java/org/mooc/cloud/MicroServiceGatewayApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @author
  */
 @SpringBootApplication
+//@EnableEurekaClient
+//@EnableHystrix
 public class MicroServiceGatewayApplication {
 
 	public static void main(String[] args) {
