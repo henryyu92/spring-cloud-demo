edfa5c41cbe5f9ed31a9bb085a1dc7d1beca4415
hernyyu92 <1401802687@qq.com>
2020-06-28 21:56:30 +0800

    update

--- microservice-starter/src/main/java/example/web/servlet/WebConfig.java
+++ microservice-starter/src/main/java/example/web/servlet/WebConfig.java
@@ -1,13 +1,27 @@
 package example.web.servlet;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> handlers) {
 
     }
 }
