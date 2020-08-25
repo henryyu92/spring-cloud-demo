46a2c918d1542095abe520f0968a3e1f29510edb
henryyu92 <1401802687@qq.com>
2020-08-26 00:03:36 +0800

    update

--- microservice-starter/microservice-component/src/main/java/example/util/DateUtil.java
+++ microservice-starter/microservice-component/src/main/java/example/util/DateUtil.java
@@ -1,6 +1,28 @@
 package example.util;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoPeriod;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+import static java.time.temporal.ChronoField.DAY_OF_YEAR;
+
 public class DateUtil {
 
+    private static TemporalAdjuster nextDay = (temporal) -> temporal.with(DAY_OF_YEAR, temporal.range(DAY_OF_YEAR).getMaximum());
+    private static TemporalAdjuster nextDayTime = (temporal) ->{temporal.with(DAY_OF_YEAR, temporal.range(DAY_OF_YEAR).getMaximum())}
 
+    public long current() {
+        return Instant.now().toEpochMilli();
+    }
+
+    public LocalDate lastDayOfMonth(LocalDate current){
+        return current.with(TemporalAdjusters.lastDayOfYear());
+    }
+
+    public LocalDateTime nextDay(LocalDateTime current){
+        return current.with(nextDayTime);
+    }
 }
