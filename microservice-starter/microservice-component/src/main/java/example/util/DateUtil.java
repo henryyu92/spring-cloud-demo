aba35a5f68f6612da0f9531191354cf8021d5cfe
henryyu92 <1401802687@qq.com>
2020-08-26 18:24:42 +0800

    update

--- microservice-starter/microservice-component/src/main/java/example/util/DateUtil.java
+++ microservice-starter/microservice-component/src/main/java/example/util/DateUtil.java
@@ -3,7 +3,6 @@ package example.util;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.chrono.ChronoPeriod;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 
@@ -12,12 +11,10 @@ import static java.time.temporal.ChronoField.DAY_OF_YEAR;
 public class DateUtil {
 
     private static TemporalAdjuster nextDay = (temporal) -> temporal.with(DAY_OF_YEAR, temporal.range(DAY_OF_YEAR).getMaximum());
-    private static TemporalAdjuster nextDayTime = (temporal) ->{temporal.with(DAY_OF_YEAR, temporal.range(DAY_OF_YEAR).getMaximum())}
+    private static TemporalAdjuster nextDayTime = (temporal) ->temporal.with(DAY_OF_YEAR, temporal.range(DAY_OF_YEAR).getMaximum());
 
-    public long current() {
-        return Instant.now().toEpochMilli();
-    }
 
+    // localDate & localTime & localDateTime
     public LocalDate lastDayOfMonth(LocalDate current){
         return current.with(TemporalAdjusters.lastDayOfYear());
     }
@@ -25,4 +22,18 @@ public class DateUtil {
     public LocalDateTime nextDay(LocalDateTime current){
         return current.with(nextDayTime);
     }
+
+    // Instant & Clock
+    public long current() {
+        return Instant.now().toEpochMilli();
+    }
+
+    // Duration & Period
+
+
+    // ZoneId & ZoneOffset & ZonedDateTime
+
+
+
+    // DateTimeFormatter
 }
