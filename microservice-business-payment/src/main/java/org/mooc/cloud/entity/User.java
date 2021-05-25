0d094a236193fc365f7abf5c78ecd333701a7e8a
henryyu92 <1401802687@qq.com>
2021-05-25 15:16:30 +0800

    update

--- microservice-business-payment/src/main/java/org/mooc/cloud/entity/User.java
+++ microservice-business-payment/src/main/java/org/mooc/cloud/entity/User.java
@@ -1,17 +1,5 @@
 package org.mooc.cloud.entity;
 
-
-import java.io.Serializable;
-
-public class User implements Serializable {
-
-    private Long id;
-
-    private String userName;
-
-    private String realName;
-
-    private int balance;
-
-
+public class User {
+    
 }
